9374144ad26c8077b221810d2b2aaf33d3a3ad5e
yinyangguaiji <77478100+yinyangguaiji@users.noreply.github.com>
2021-01-27 22:40:30 +0800

    习题

---
+++ 3-10.py
@@ -0,0 +1,22 @@
+things = ['CSGO','Mount_Tai','China','ukpkmkk']
+print(things)
+things.append('Left_4_Dead')
+print(things)
+things.insert(2,'Half_Life')
+print(things)
+del things[1]
+print(things)
+print(things.pop(0))
+things.remove('Left_4_Dead')
+print(things)
+print(sorted(things))
+things.sort()
+print(things)
+things.reverse()
+print(things)
+print(len(things))
+
+
+
+
+
